1339e249cdc257c2febb9e93eeb1ca9871b2f6b9
nisherahman <frozennishe@gmail.com>
2021-12-24 15:08:50 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
-    //slick js
+
+    // scroll position
+    $(window).on('scroll',function(){
+      var scrolling = $(this).scrollTop();
+  
+    // back to top appearance 
+    if(scrolling > 100){
+        $('.back-to-top').fadeIn();
+    }else{
+        $('.back-to-top').fadeOut();  
+    }
+    // navbar class 
+    if(scrolling > 150){
+        $('nav').addClass('nav-fix');
+    }else{
+        $('nav').removeClass('nav-fix');
+    }
+
+    });
+
+    // back to top action 
+    $('.back-to-top').on('click',function(){
+      $('html,body').animate({
+        scrollTop: '0px',
+    },2000);
+
+    });
+
+    //banner slider
     $('.banner-slider').slick({
         arrows: false,
         // autoplay:true,
         dots:true,
         speed:700,
     });
+    
     //about video popup
     $('.venobox').venobox();
     
